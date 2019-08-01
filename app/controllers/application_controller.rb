63ea4663343caaa96e3e7c43a4771fb2fe0dde7b
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-08-01 21:27:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
-  # get '/' do
-  #   erb :super_hero
-  # end
+  get '/' do
+    erb :super_hero
+  end
 
   post '/teams' do
     @teams = Team.new(params[:name][:motto])
